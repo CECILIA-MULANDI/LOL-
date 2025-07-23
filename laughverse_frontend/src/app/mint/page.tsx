0d3b1efed2e059f24223494d5e34a22cac52ec0d
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-23 23:53:01 +0300

    perf: optimize file upload process

--- laughverse_frontend/src/app/mint/page.tsx
+++ laughverse_frontend/src/app/mint/page.tsx
@@ -296,3 +296,5 @@ export default function MintPage() {
 // Auto-commit whitespace: 2025-07-23 16:38:02
 // Auto-commit whitespace: 2025-07-23 16:39:04
 // Auto-commit whitespace: 2025-07-23 16:45:19
+
+// Auto-commit spacing update: 2025-07-23 23:53:01
