9fbf0a8b23d01e9b908e4b26d5f10b060bf87881
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-23 16:47:23 +0300

    style: update UI components

--- laughverse_frontend/src/app/utils/ipfs.ts
+++ laughverse_frontend/src/app/utils/ipfs.ts
@@ -114,3 +114,4 @@ export const uploadMetadataToIPFS = async (
 // Auto-commit whitespace: 2025-07-23 16:40:06
 
 // Auto-commit spacing update: 2025-07-23 16:42:12
+// Auto-commit whitespace: 2025-07-23 16:47:23
