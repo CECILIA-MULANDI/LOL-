eeedaddb71229e5cdeab09ada5b740f8641ff1d5
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-24 18:46:43 +0300

    feat: add transaction status tracking

--- laughverse_frontend/src/app/smart-contracts/constants.ts
+++ laughverse_frontend/src/app/smart-contracts/constants.ts
@@ -1,3 +1,4 @@
 export const CONTRACT_ADDRESS = "0x5f50935eA06250AF8a2a049913DbDFeB2F8629AE";
 // Auto-commit whitespace: 2025-07-24 18:35:53
 // Auto-commit whitespace: 2025-07-24 18:41:31
+// Auto-commit whitespace: 2025-07-24 18:46:43
