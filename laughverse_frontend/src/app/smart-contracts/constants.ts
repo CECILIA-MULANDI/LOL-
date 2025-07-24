1d508975581a22495528311026829a5bb60de01f
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-24 18:47:45 +0300

    chore: clean up code structure

--- laughverse_frontend/src/app/smart-contracts/constants.ts
+++ laughverse_frontend/src/app/smart-contracts/constants.ts
@@ -1,3 +1,4 @@
+// Auto-commit update: 2025-07-24 18:47:45
 export const CONTRACT_ADDRESS = "0x5f50935eA06250AF8a2a049913DbDFeB2F8629AE";
 // Auto-commit whitespace: 2025-07-24 18:35:53
 // Auto-commit whitespace: 2025-07-24 18:41:31
