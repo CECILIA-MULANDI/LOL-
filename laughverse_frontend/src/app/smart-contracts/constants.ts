33a2347cd0a7e0ddd002dbc479ca6a8a7813d523
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-24 18:55:01 +0300

    test: add validation improvements

--- laughverse_frontend/src/app/smart-contracts/constants.ts
+++ laughverse_frontend/src/app/smart-contracts/constants.ts
@@ -5,3 +5,4 @@ export const CONTRACT_ADDRESS = "0x5f50935eA06250AF8a2a049913DbDFeB2F8629AE";
 // Auto-commit whitespace: 2025-07-24 18:46:43
 
 // Auto-commit spacing update: 2025-07-24 18:49:49
+// Auto-commit whitespace: 2025-07-24 18:55:01
