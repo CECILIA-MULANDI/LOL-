d6c54445727d42f9a1340fa7c9856c552187b6ac
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-25 23:44:32 +0300

    perf: optimize file upload process

--- laughverse_frontend/src/app/smart-contracts/constants.ts
+++ laughverse_frontend/src/app/smart-contracts/constants.ts
@@ -24,3 +24,5 @@ export const CONTRACT_ADDRESS = "0x5f50935eA06250AF8a2a049913DbDFeB2F8629AE";
 // Auto-commit spacing update: 2025-07-25 23:38:22
 
 // Auto-commit spacing update: 2025-07-25 23:41:27
+
+// Auto-commit spacing update: 2025-07-25 23:44:32
