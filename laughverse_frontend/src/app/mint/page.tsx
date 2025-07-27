7febdcf5a24878685f62acb83080bd1de852e4cb
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-27 16:27:10 +0300

    chore: update dependencies and config

--- laughverse_frontend/src/app/mint/page.tsx
+++ laughverse_frontend/src/app/mint/page.tsx
@@ -369,3 +369,4 @@ export default function MintPage() {
 // Auto-commit spacing update: 2025-07-26 13:05:09
 // Auto-commit whitespace: 2025-07-27 16:22:49
 // Auto-commit whitespace: 2025-07-27 16:25:03
+// Auto-commit whitespace: 2025-07-27 16:27:10
