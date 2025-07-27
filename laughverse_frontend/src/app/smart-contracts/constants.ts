9d658c9d323a13718272eb9a6151b7bbc181d37a
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-27 16:23:55 +0300

    refactor: improve metadata handling

--- laughverse_frontend/src/app/smart-contracts/constants.ts
+++ laughverse_frontend/src/app/smart-contracts/constants.ts
@@ -31,3 +31,5 @@ export const CONTRACT_ADDRESS = "0x5f50935eA06250AF8a2a049913DbDFeB2F8629AE";
 // Auto-commit spacing update: 2025-07-26 12:47:41
 
 // Auto-commit spacing update: 2025-07-26 12:51:48
+
+// Auto-commit spacing update: 2025-07-27 16:23:55
