d80ddfd7b53f18a4c783d795ec29c9d5fda309bb
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-07-27 16:31:26 +0300

    feat: enhance mint page functionality

--- laughverse_frontend/src/app/mint/page.tsx
+++ laughverse_frontend/src/app/mint/page.tsx
@@ -370,3 +370,4 @@ export default function MintPage() {
 // Auto-commit whitespace: 2025-07-27 16:22:49
 // Auto-commit whitespace: 2025-07-27 16:25:03
 // Auto-commit whitespace: 2025-07-27 16:27:10
+// Auto-commit whitespace: 2025-07-27 16:31:26
